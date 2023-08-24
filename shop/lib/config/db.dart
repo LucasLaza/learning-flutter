e004d19afd436dcb84eb72bf926c83776d828d15
cristianekoester <cristianekoester@live.com>
2023-08-24 17:55:49 -0300

    implemented product registration and firebase connection

---
+++ shop/lib/config/db.dart
@@ -0,0 +1,6 @@
+class DBConfig {
+  static const productBaseUrl =
+      'https://shop-cod3r-de827-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-cod3r-de827-default-rtdb.firebaseio.com/orders';
+}
