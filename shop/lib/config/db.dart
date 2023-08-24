172e6c019f97e26fa2d488802d7e1b398ed2493b
cristianekoester <cristianekoester@live.com>
2023-08-24 17:59:56 -0300

    removing config folder from repo

--- shop/lib/config/db.dart
+++
@@ -1,6 +0,0 @@
-class DBConfig {
-  static const productBaseUrl =
-      'https://shop-cod3r-de827-default-rtdb.firebaseio.com/products';
-  static const orderBaseUrl =
-      'https://shop-cod3r-de827-default-rtdb.firebaseio.com/orders';
-}
