be6b50d567bde43580fc1952eb613cf178ed0834
cristianekoester <cristianekoester@live.com>
2023-08-25 16:48:37 -0300

    implemented authentication
    implemented login and auth for users

--- shop/lib/models/product.dart
+++ shop/lib/models/product.dart
@@ -26,13 +26,15 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavorite() async {
+  Future<void> toggleFavorite(String token, String userId) async {
     try {
       _toggleFavorite();
 
-      final response = await http.patch(
-        Uri.parse('${DBConfig.productBaseUrl}/$id.json'),
-        body: jsonEncode({"isFavorite": isFavorite}),
+      final response = await http.put(
+        Uri.parse(
+          '${DBConfig.userFavoritesUrl}/$userId/$id.json?auth=$token',
+        ),
+        body: jsonEncode(isFavorite),
       );
 
       if (response.statusCode >= 400) {
